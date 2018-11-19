0ebf6ecd5dd0302f8fa61c73a4f6d65b5a0633db
JavaBased <1813697196@qq.com>
2018-11-19 18:27:29 +0800

    Test

---
+++ src/com/FirstSeason/JavaBased/Demo1120.java
@@ -0,0 +1,5 @@
+package com.FirstSeason.JavaBased;
+
+public class Demo1120 {
+
+}
---
+++ src/com/FirstSeason/JavaBased/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.FirstSeason.JavaBased;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("I LOVE CC");
+	}
+
+}
