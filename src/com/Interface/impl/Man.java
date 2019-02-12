d1d2deef42e65151e856dc45ed9df03b4aa225ec
JavaBased <1813697196@qq.com>
2019-02-12 11:15:58 +0800

    Interface

---
+++ src/com/Interface/impl/Man.java
@@ -0,0 +1,13 @@
+package com.Interface.impl;
+
+public class Man implements Person{
+	public void run() {
+		
+	}
+	public void eat() {
+		
+	}
+	public void talk() {
+		
+	}
+}
---
+++ src/com/Interface/impl/Person.java
@@ -0,0 +1,8 @@
+package com.Interface.impl;
+//定义简单的接口
+public interface Person {
+	public void run();
+	public void eat();
+	public void talk();
+
+}
